0e4b09faba96cfe0c5bcc7ca802790b687142ddf
Josh <josh_hill15@me.com>
2020-05-08 13:24:45 -0400

    mvp

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,21 @@
-def get_indices_of_item_weights(weights, length, limit):
 
+def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
+    O(n)
     """
+    hm = {}
+    a = length - 1
+    for i in range(length//2):
+        if weights[a] + weights[i] == limit:
+            print(a, weights[a], i, weights[i])
+            hm[weights[i] + weights[a]] = (a, i)
+        a -= 1
+    if limit in hm:
+        return hm[limit]
+    else:
+        return None
 
-    return None
+
+# print(get_indices_of_item_weights([4, 6, 10, 15, 16], 5, 21))  # [3,1]
+print(get_indices_of_item_weights([9], 1, 9))  # None
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -6,9 +6,41 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    
     """
     YOUR CODE HERE
     """
+    arr = []
+    hm = {}
+    for ticket in tickets:
+        if ticket not in hm:
+            hm[ticket.source] = ticket.destination
+    # while len(arr) < 1:
+    key = "NONE"
+    while key is not None:
+        if key is "NONE":
+            arr.append(hm[key])
+            key = hm[key]
 
-    return route
+        if len(arr) >= 1:
+            arr.append(hm[key])
+            key = hm[key]
+
+        if len(arr) == length-1:
+            key = None
+
+    return arr
+
+
+tickets = [
+    Ticket("PIT", "ORD"),
+    Ticket("XNA", "CID"),
+    Ticket("SFO", "BHM"),
+    Ticket("FLG", "XNA"),
+    Ticket("NONE", "LAX"),
+    Ticket("LAX", "SFO"),
+    Ticket("CID", "SLC"),
+    Ticket("ORD", "NONE"),
+    Ticket("SLC", "PIT"),
+    Ticket("BHM", "FLG")
+]
+print(reconstruct_trip(tickets, len(tickets)))
